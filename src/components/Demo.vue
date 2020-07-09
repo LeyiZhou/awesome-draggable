2d14910a7b6382d1d838c5f0303c4752c585e0b6
Leyi <1005849273@qq.com>
2020-07-09 13:49:12 +0800

    init

---
+++ src/components/Demo.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="demo">
+    <div class="draggable">
+      <draggable v-model="list">
+        <transition-group>
+          <div v-for="(item, index) in list" :key="index" class="item">{{item}}</div>
+        </transition-group>
+      </draggable>
+    </div>
+  </div>
+</template>
+
+<script>
+import draggable from 'vuedraggable'
+export default {
+  name: 'Demo',
+  components: {
+    draggable
+  },
+  data () {
+    return {
+      list: ['Leyi', 'so', 'cool']
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.demo {
+  background: #e9ecef;
+  height: 100vh;
+  .draggable {
+    margin: 0 auto;
+    .item {
+      width: 300px;
+      background: #fff;
+      color: #000;
+    }
+  }
+}
+</style>
